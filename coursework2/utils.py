589c39484f39a909b89dc2c86d4f32561716a1a2
asikeero <60272147+asikeero@users.noreply.github.com>
2022-11-29 21:39:34 +0900

    update

---
+++ coursework2/utils.py
@@ -0,0 +1,52 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython import display
+
+import torch
+import torchvision.utils as utils
+import torch.nn.functional as F
+import torch.nn as nn
+
+
+def save_model(model, filename, confirm=True):
+    if confirm:
+        try:
+            save = input('Do you want to save the model (type yes to confirm)? ').lower()
+            if save != 'yes':
+                print('Model not saved.')
+                return
+        except:
+            raise Exception('The notebook should be run or validated with skip_training=True.')
+
+    torch.save(model.state_dict(), filename)
+    print('Model saved to %s.' % (filename))
+
+
+def load_model(model, filename, device):
+    model.load_state_dict(torch.load(filename, map_location=lambda storage, loc: storage))
+    print('Model loaded from %s.' % filename)
+    model.to(device)
+    model.eval()
+
+def plot_images(images, ncol=12, figsize=(8,8), cmap=plt.cm.Greys, clim=[0,1]):
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.axis('off')
+    grid = utils.make_grid(images, nrow=ncol, padding=0, normalize=False).cpu()
+    ax.imshow(grid[0], cmap=cmap, clim=clim)
+    display.display(fig)
+    plt.close(fig)
+
+
+def plot_generated_samples(samples, ncol=12, savename=None):
+    fig, ax = plt.subplots(figsize=(8,8))
+    ax.axis('off')
+    d = np.transpose(
+            torchvision.utils.make_grid(samples, nrow=ncol, padding=0, normalize=True).cpu(),
+            (1,2,0)
+        )
+    ax.imshow(d)
+    if savename:
+        plt.imsave(f'images/{savename}', d.numpy())
+    display.display(fig)
+    plt.close(fig)
